217487ea3a61708d99b92387e85bbe86e45c6280
Srikanth <srikanth.k@zohocorp.com>
2025-06-05 08:38:54 +0530

    Added array problem 1

---
+++ problem-1/Problem.java
@@ -0,0 +1,9 @@
+public class Problem {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
+
