23cd1f382d0faa37d6a57820e805712e9d257544
Srikanth <srikanth.k@zohocorp.com>
2025-06-06 13:29:38 +0530

    problem-2 typecastin

---
+++ problem-2/Typecasting.java
@@ -0,0 +1,10 @@
+public class Typecasting {
+    public static void main(String args []){
+        float num = 99.99f;
+        int newNum=(int)num;
+        double newDoubleNum=(double)newNum;
+        System.out.println("float value : "+num+", int value : "+newNum+", double value : "+newDoubleNum);
+
+
+    }
+}
