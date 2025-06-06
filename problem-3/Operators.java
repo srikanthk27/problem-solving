625625a1a92732e2158fc2f08db1299099a201cb
Srikanth <srikanth.k@zohocorp.com>
2025-06-06 13:41:08 +0530

    problem-3 operators added

---
+++ problem-3/Operators.java
@@ -0,0 +1,26 @@
+public class Operators {
+    public static void main(String[] args) {
+         int a = 10;
+         int b = 20;
+         
+
+         System.out.println("a+b = "+(a+b));
+         System.out.println("a-b = "+(a-b));
+         System.out.println("b-a = "+(b-a));
+         System.out.println("a*b = "+(a*b));
+         System.out.println("a%b = "+(a%b));
+         System.out.println("b%a = "+(b%a));
+         System.out.println("b/a = "+(b/a));
+         System.out.println((b>a));
+         System.out.println((b>a));
+         System.out.println((b>5 &&a>3));
+         System.out.println((b<5 || a>3));
+         System.out.println("a = "+(a+=b));
+
+
+
+
+
+
+    }
+}
